b7663b0d739d454b1c3fae22888f447852c8605e
Nathan Florencio <nflorencio@gmail.com>
2022-08-09 14:56:16 -0300

    Making the game works.

--- src/App.tsx
+++ src/App.tsx
@@ -17,8 +17,8 @@ const App = () => {
   const [playing, setPlaying] = useState<boolean>(false);
   const [timeElapsed, setTimeElapsed] = useState<number>(0);
   const [moveCount, setMoveCount] = useState<number>(0);
-  const [showCount, setShowCount] = useState<number>(0);
-  const [gridItems, setGridItems] = useState<GridItemType[]>();
+  const [shownCount, setShownCount] = useState<number>(0);
+  const [gridItems, setGridItems] = useState<GridItemType[]>([]);
 
 
   useEffect(() => resetAndCreateGrid(), []);
@@ -30,11 +30,44 @@ const App = () => {
     return () => clearInterval(timer);
   }, [playing, timeElapsed]);
 
+  // verify if opened are equal
+  useEffect(() => {
+    if (shownCount === 2) {
+      let opened = gridItems.filter(item => item.shown === true);
+      if (opened.length === 2) {
+        if (opened[0].item === opened[1].item) {
+          // v1 - if both are equal, make every "shown" permanent
+          let tmpGrid = [...gridItems];
+          for (let i in tmpGrid) {
+            if (tmpGrid[i].shown) {
+              tmpGrid[i].permanentShown = true;
+              tmpGrid[i].shown = false;
+            }
+          }
+          setGridItems(tmpGrid);
+          setShownCount(0);
+        } else {
+          // v2 - if they are not equal, close all "shown"
+          setTimeout(() => {
+            let tmpGrid = [...gridItems];
+            for (let i in tmpGrid) {
+              tmpGrid[i].shown = false
+            }
+            setGridItems(tmpGrid);
+            setShownCount(0);
+          }, 1000);
+        }
+
+        setMoveCount(moveCount => moveCount + 1);
+      }
+    }
+  }, [shownCount, gridItems]);
+
   const resetAndCreateGrid = () => {
     // passo 1 - resetar o jogo
     setTimeElapsed(0);
     setMoveCount(0);
-    setShowCount(0);
+    setShownCount(0);
 
     // passo 2 - criar o grid e começar o jogo
     // 2.1 - criar um grid vazio
@@ -61,7 +94,14 @@ const App = () => {
   }
 
   const handleItemClick = (index: number) => {
-
+    if (playing && index !== null && shownCount < 2) {
+      let tmpGrid = [...gridItems];
+      if (tmpGrid[index].permanentShown === false && tmpGrid[index].shown === false) {
+        tmpGrid[index].shown = true;
+        setShownCount(shownCount + 1);
+      }
+      setGridItems(tmpGrid);
+    }
   }
 
   return (
@@ -73,14 +113,14 @@ const App = () => {
 
         <C.InfoArea>
           <InfoItem label='Tempo' value={formatTimeElapsed(timeElapsed)} />
-          <InfoItem label='Movimentos' value='0' />
+          <InfoItem label='Movimentos' value={moveCount.toString()} />
         </C.InfoArea>
 
         <Button label='Reiniciar' icon={RestartIcon} onClick={resetAndCreateGrid} />
       </C.Info>
       <C.GridArea>
         <C.Grid>
-          {gridItems?.map((item, index) => (
+          {gridItems.map((item, index) => (
             <GridItem
               key={index}
               item={item}
