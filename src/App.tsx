4a8839ab0e8449ba15ee40dc2c07192d8ea22deb
Nathan Florencio <nflorencio@gmail.com>
2022-08-09 12:48:45 -0300

    Created logical Grid Area.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,50 @@
+import { useEffect, useState } from 'react';
 import * as C from './App.styles';
 import logoImage from './assets/devmemory_logo.png';
 import RestartIcon from './svgs/restart.svg';
 import { Button } from './components/Button';
 import { InfoItem } from './components/InfoItem';
+import { GridItemType } from './types/GridItemType';
+import { items } from './data/items';
 
 const App = () => {
+  const [playing, setPlaying] = useState<boolean>(false);
+  const [timeElapsed, setTimeElapsed] = useState<number>(0);
+  const [moveCount, setMoveCount] = useState<number>(0);
+  const [showCount, setShowCount] = useState<number>(0);
+  const [gridItems, setGridItems] = useState<GridItemType[]>();
+
+
+  useEffect(() => resetAndCreateGrid(), []);
 
   const resetAndCreateGrid = () => {
+    // passo 1 - resetar o jogo
+    setTimeElapsed(0);
+    setMoveCount(0);
+    setShowCount(0);
 
+    // passo 2 - criar o grid e começar o jogo
+    // 2.1 - criar um grid vazio
+    let tmpGrid: GridItemType[] = [];
+    for (let i = 0; i < (items.length * 2); i++) tmpGrid.push({
+      item: null, shown: false, permanentShown: false
+    });
+    // 2.2 - preencher o grid
+    for (let w = 0; w < 2; w++) {
+      for (let i = 0; i < items.length; i++) {
+        let pos = -1;
+        while (pos < 0 || tmpGrid[pos].item !== null) {
+          pos = Math.floor(Math.random() * (items.length * 2));
+        }
+        tmpGrid[pos].item = i;
+      }
+    }
+
+    // 2.3 - jogar no state
+    setGridItems(tmpGrid);
+
+    // passo 3 - começar o jogo
+    setPlaying(true);
   }
 
   return (
@@ -25,7 +62,9 @@ const App = () => {
         <Button label='Reiniciar' icon={RestartIcon} onClick={resetAndCreateGrid} />
       </C.Info>
       <C.GridArea>
-        ...
+        <C.Grid>
+
+        </C.Grid>
       </C.GridArea>
     </C.Container>
   );
